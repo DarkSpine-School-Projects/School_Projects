52e402ec6eebd1e1ee4688b6bc4e73d15504b386
PracowniaRepo <pracownia_repo@outlook.com>
2023-02-07 13:24:35 +0100

    update

--- PracowniaRepo/own_project/index.php
+++ PracowniaRepo/own_project/index.php
@@ -6,9 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OwnProject</title>
     <link rel="stylesheet" href="./assets/style/Main_style/style.css">
+    <link rel="stylesheet" href="./assets/style/index_style/index.css">
 </head>
 <body>
+    <?php
+    
+        mysqli_connect('127.0.0.1','root_DB','','test_db');
+    
+    ?>
     <!-- Menu Bar -->
-    <?php include './assets/site/menu.php' ?>
+    <?php include './assets/components/nav.php' ?>
 </body>
 </html>
