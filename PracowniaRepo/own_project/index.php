f3dec8cc3b830721591ae293249a9cfdcf0594a3
PracowniaRepo <pracownia_repo@outlook.com>
2023-02-27 12:03:22 +0100

    fdgdfg

--- PracowniaRepo/own_project/index.php
+++ PracowniaRepo/own_project/index.php
@@ -5,14 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OwnProject</title>
-    <link rel="stylesheet" href="./assets/style/Main_style/style.css">
+
     <link rel="stylesheet" href="./assets/style/index_style/index.css">
 </head>
 <body>
+    <div class="load-container">
+        <div class="load-animation">
+            <span class="word-load word-load-1">asd</span>
+            <span class="word-load word-load-1">asd</span>
+            <span class="word-load word-load-1">asd</span>
+            <span class="word-load word-load-1">asd</span>
+        </div>
+    </div>
     <?php
-    
         mysqli_connect('127.0.0.1','root_DB','','test_db');
-    
     ?>
     <!-- Menu Bar -->
     <?php include './assets/components/nav.php' ?>
