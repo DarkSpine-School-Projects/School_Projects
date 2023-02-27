8056e3cca8641ca097ed669bfaea8bd7f9cd7059
PracowniaRepo <pracownia_repo@outlook.com>
2023-02-27 12:34:44 +0100

    update

--- PracowniaRepo/own_project/index.php
+++ PracowniaRepo/own_project/index.php
@@ -9,13 +9,11 @@
     <link rel="stylesheet" href="./assets/style/index_style/index.css">
 </head>
 <body>
-    <div class="load-container">
-        <div class="load-animation">
-            <span class="word-load word-load-1">asd</span>
-            <span class="word-load word-load-1">asd</span>
-            <span class="word-load word-load-1">asd</span>
-            <span class="word-load word-load-1">asd</span>
-        </div>
+    <div class="load-container">  
+            <span class="word-load word-load-1">Największa</span>
+            <span class="word-load word-load-1">Biblioteka</span>
+            <span class="word-load word-load-1">Filmów</span>
+            
     </div>
     <?php
         mysqli_connect('127.0.0.1','root_DB','','test_db');
