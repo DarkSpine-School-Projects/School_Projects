87bb9f215fb1d4a9dc0ac86cba1968e779350302
Dawid Konopiaty <darkspine.33@gmail.com>
2023-02-28 22:28:47 +0100

    update

--- PracowniaRepo/own_project/assets/components/nav.php
+++ PracowniaRepo/own_project/assets/components/nav.php
@@ -17,8 +17,17 @@
             option
         </li>
         <li>
-            <input type="text" name="search" class="search">
-            <button class="search-btn">SEARCH</button>
+            <form action="POST">
+                <input type="text" name="search" class="search">
+                <button type='sumbit' name='search' class="search-btn">SEARCH</button>
+            </form>
+            <?php 
+            if(isset($search)){
+                $search=$_POST['search'];
+                $sql = "SELECT * FROM `movie` WHERE name like '$search%';";
+            };
+            
+            ?>
         </li>
     </ul>
 </nav>
