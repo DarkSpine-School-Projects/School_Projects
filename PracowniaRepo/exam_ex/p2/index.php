67d904b04e798092b636198f9dc49ea988c1e711
Dawid Konopiaty <dawidkonopiaty@gmail.com>
2023-09-19 17:07:57 +0200

    finish p2

--- PracowniaRepo/exam_ex/p2/index.php
+++ PracowniaRepo/exam_ex/p2/index.php
@@ -1,62 +1,100 @@
 <!DOCTYPE html>
 <html lang="pl">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sekretariat</title>
     <link rel="stylesheet" href="styl.css">
-    <?php $conn=mysqli_connect('localhost', 'root', '', 'firma');?>
+    <?php $conn = mysqli_connect('localhost', 'root', '', 'firma'); ?>
     <?php require './zapytania.php' ?>
 </head>
+
 <body>
     <div id="lewy">
         <h1>Akta Pracownicze</h1>
-        <?php 
-
+        <?php
         //script 1
-        echo'<h3>Dane</h3>';
 
-        $query = mysqli_query($conn,$zapytania[0]);
-        while($fetch= mysqli_fetch_array($query)){
-            echo'<p>'.$fetch[0].' '.$fetch[1].'</p>';
+        echo '<h3>Dane</h3>';
+
+        $query = mysqli_query($conn, $zapytania[0]);
+        while ($fetch = mysqli_fetch_array($query)) {
+            echo '<p>' . $fetch[0] . ' ' . $fetch[1] . '</p>';
         };
 
-        echo'<hr>';
-        echo'<h3>adres</h3>';
+        echo '<hr>';
+        echo '<h3>adres</h3>';
 
-        $query = mysqli_query($conn,$zapytania[0]);
-        while($fetch = mysqli_fetch_array($query)){
-            echo'<p>'.$fetch[2].'</p>';
-            echo'<p>'.$fetch[3].'</p>';
-
+        $query = mysqli_query($conn, $zapytania[0]);
+        while ($fetch = mysqli_fetch_array($query)) {
+            echo '<p>' . $fetch[2] . '</p>';
+            echo '<p>' . $fetch[3] . '</p>';
         };
-        echo'<hr>';
-        $query = mysqli_query($conn,$zapytania[0]);
+        echo '<hr>';
+        $query = mysqli_query($conn, $zapytania[0]);
         echo '<p>Rodo:';
-        while($fetch = mysqli_fetch_array($query)){
-            if($fetch[4]=1){
+        while ($fetch = mysqli_fetch_array($query)) {
+            if ($fetch[4] = 1) {
                 echo ' Podpisano';
-            }else{
+            } else {
                 echo ' Nie pdopisano';
             }
         };
-        echo'</p>';
-        $query = mysqli_query($conn,$zapytania[0]);
+        echo '</p>';
+        $query = mysqli_query($conn, $zapytania[0]);
         echo '<p>Badania:';
-        while($fetch = mysqli_fetch_array($query)){
-            if($fetch[5]=1){
+        while ($fetch = mysqli_fetch_array($query)) {
+            if ($fetch[5] = 1) {
                 echo ' Podpisano';
-            }else{
+            } else {
                 echo ' Nie pdopisano';
             }
         };
-        echo'</p>';
+        echo '</p>';
         ?>
         <hr>
         <h3>Dokmenty pracownika</h3>
         <a href="./cv.txt" download>pobierz</a>
+        <h1>Liczba zatrudionych pracownikow</h1>
+        <?php
+        //script 2
+
+        $query = mysqli_query($conn, $zapytania[1]);
+        $fetch = mysqli_fetch_array($query);
+        echo '<p>' . $fetch[0] . '</p>';
+        ?>
     </div>
-    <div id="prawy"></div>
-    <div id="stopka"></div>
+    <div id="prawy">
+
+        <?php
+        //script 3
+
+        $query = mysqli_query($conn, $zapytania[2]);
+        $fetch = mysqli_fetch_array($query);
+        echo '<img src="' . $fetch[0] . '.jpg" alt="pracownik"/>';
+
+        $query = mysqli_query($conn, $zapytania[2]);
+        while ($fetch = mysqli_fetch_array($query)) {
+            echo '<h2>' . $fetch[1] . ' ' . $fetch[2] . '</h2>';
+        };
+
+
+        $query = mysqli_query($conn, $zapytania[3]);
+        while ($fetch = mysqli_fetch_array($query)) {
+            echo '<h4>' . $fetch[1] . '</h4>';
+            echo '<h5>' . $fetch[2] . '</h5>';
+        };
+        ?>
+
+    </div>
+    <div id="stopka">
+        Autorem aplikacji jest: XYZ
+        <ul>
+            <li>skontaktuj sie</li>
+            <li>poznaj nasza firme</li>
+        </ul>
+    </div>
 </body>
+
 </html>
--- PracowniaRepo/exam_ex/p2/zapytania.php
+++ PracowniaRepo/exam_ex/p2/zapytania.php
@@ -1,21 +1,13 @@
-<?php 
+<?php
 
 
 
 $zapytania = [
-    "SELECT `imie`, `nazwisko`, `adres`, `miasto`, `czyRODO`, `czyBadania` FROM `pracownicy` WHERE id = 2;",//kw1
+    "SELECT `imie`, `nazwisko`, `adres`, `miasto`, `czyRODO`, `czyBadania` FROM `pracownicy` WHERE id = 2;", //kw1
 
-    "SELECT COUNT(*) FROM `pracownicy`;",//kw2
+    "SELECT COUNT(*) FROM `pracownicy`;", //kw2
 
-    "SELECT `id`, `imie`, `nazwisko` FROM `pracownicy` WHERE id = 2;",//kw3
+    "SELECT `id`, `imie`, `nazwisko` FROM `pracownicy` WHERE id = 2;", //kw3
 
-    "SELECT pracownicy.id, stanowiska.nazwa, stanowiska.opis FROM `pracownicy`  INNER JOIN  `stanowiska`  WHERE pracownicy.stanowiska_id = stanowiska.id and pracownicy.id = 2:"//kw4
+    "SELECT pracownicy.id, stanowiska.nazwa, stanowiska.opis FROM `pracownicy`  INNER JOIN  `stanowiska`  WHERE pracownicy.stanowiska_id = stanowiska.id and pracownicy.id = 2;" //kw4
 ];
-
-?>
-
-
-
-
-
-
