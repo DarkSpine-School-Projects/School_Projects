25e8c8e9ab7fe6469e9acbb69d124fafb9df7925
PracowniaRepo <pracownia_repo@outlook.com>
2023-10-12 13:23:35 +0200

    update

--- PracowniaRepo/own_project_1/app/site/auth/login.php
+++ PracowniaRepo/own_project_1/app/site/auth/login.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-
+    <link rel="stylesheet" href="./style.css">
     <?php
     require_once '/xampp_8.2.3/htdocs/Projects_Done_On_Lessons/PracowniaRepo/own_project_1/db/connect.php';
     ?>
@@ -17,132 +17,6 @@
         die();
     }
     ?>
-    <style>
-    @import url('https://fonts.googleapis.com/css2?family=Pixelify+Sans&display=swap');
-
-    * {
-        font-family: 'Pixelify Sans', sans-serif;
-    }
-
-    body {
-        background: rgb(0, 0, 0);
-        background: linear-gradient(29deg,
-                rgb(0, 0, 0) 11%,
-                rgb(46, 46, 46) 50%,
-                rgb(0, 0, 0) 90%);
-    }
-
-    #reg {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translateX(-50%) translateY(-50%);
-        padding: 0px 40px;
-        height: 70dvh;
-        background: red;
-        display: flex;
-        flex-direction: column;
-        justify-items: left;
-        align-content: left;
-        justify-content: center;
-        align-items: center;
-        background: rgb(0, 0, 0);
-        background: linear-gradient(29deg,
-                rgb(0, 0, 0) 11%,
-                rgb(46, 46, 46) 50%,
-                rgb(0, 0, 0) 90%);
-        border-radius: 16px;
-        box-shadow: 0px 0px 100px gray;
-        border: 1px solid rgba(255, 255, 255, 0.11);
-    }
-
-    .parent_strength {
-        border: 1px solid red;
-        height: 10px;
-        width: 70%;
-    }
-
-    #strenght_pwd {
-        background: transparent;
-        height: 100%;
-        width: 0%;
-        transition: all 0.2s ease-in-out;
-    }
-
-    #pwd2,
-    #pwd3,
-    #pwd4,
-    #pwd5,
-    #pwd6,
-    #pwd7,
-    #pwd8 {
-        display: inline;
-        font-weight: bold;
-        color: burlywood;
-        text-transform: uppercase;
-        letter-spacing: 2px;
-    }
-
-    .impo {
-        font-size: 30px;
-        text-transform: uppercase;
-        font-weight: bold;
-        animation: col 0.5s ease-in-out alternate infinite;
-    }
-
-    @keyframes col {
-        0% {
-            color: white;
-        }
-
-        100% {
-            color: red;
-        }
-    }
-
-
-    .pwd_input {
-        border: none;
-        width: 10rem;
-        padding: 1px;
-        font-size: 19px;
-        transition: 0.2s ease-in-out;
-    }
-
-    .pwd_input:focus {
-        width: 80%;
-        padding: 3px;
-        border: 1px solid gray;
-        outline: none;
-
-    }
-
-
-
-    label {
-        letter-spacing: 10px;
-        color: gray;
-        font-size: large;
-    }
-
-    #sumbit {
-        background: black;
-        border: 0px;
-        padding: 5px 35px;
-        color: white;
-        transition: 0.2s ease-in-out;
-        margin-top: 2vh;
-    }
-
-    #sumbit:hover {
-        background: white;
-        transform: scale(1.15);
-        color: black;
-        cursor: pointer;
-    }
-
-    /*# sourceMappingURL=style.css.map */
-    </style>
 
 </head>
 
@@ -150,7 +24,7 @@
 
 
 
-
+    <input type="button" onclick="window.history.go(-1); return false;" value="<">
     <div class="container">
 
         <form method="POST" id="reg">
@@ -164,16 +38,21 @@
             <input type="text" name="lastname" id="lastname" class="pwd_input" required>
             <br>
             <label for="pwd">Password:</label>
+
+
             <input type="password" name="pwd" id="pwd" onkeyup="check_strenght_output()" class="pwd_input" required>
-            <span>haslo ma zawierac:</span>
-            <span id="pwd3" class="checkpwd">duza litere</span>
-            <span id="pwd4" class="checkpwd">mala litera</span>
-            <span id="pwd5" class="checkpwd">cyfra</span>
-            <span id="pwd6" class="checkpwd">znak specjalny</span>
-            <span id="pwd7" class="checkpwd">więcej niż 4 znaki</span>
-            <span id="pwd8" class="checkpwd">Hasła muszą do siebie pasować</span>
+
             <label for="pwd_1">Type Password Again</label>
             <input type="password" name="pwd_1" id="pwd_1" class="pwd_input" required>
+            <div id="pwd_prompt">
+                <div>haslo ma zawierac:</div>
+                <div id="pwd3" class="checkpwd">duza litere</div>
+                <div id="pwd4" class="checkpwd">mala litera</div>
+                <div id="pwd5" class="checkpwd">cyfra</div>
+                <div id="pwd6" class="checkpwd">znak specjalny</div>
+                <div id="pwd7" class="checkpwd">więcej niż 4 znaki</div>
+                <div id="pwd8" class="checkpwd">Hasła muszą do siebie pasować</div>
+            </div>
             <br>
             <button type="submit" name="sumbit" id="sumbit" onclick="regReset()">Sumbit</button>
 
