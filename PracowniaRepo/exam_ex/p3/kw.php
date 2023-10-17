2a7b1af97d7ccaa7c19ce8ab35aa491d4bbf3ab5
Dawid Konopiaty <dawidkonopiaty@gmail.com>
2023-10-17 21:00:01 +0200

    update exam ex

--- PracowniaRepo/exam_ex/p3/kw.php
+++ PracowniaRepo/exam_ex/p3/kw.php
@@ -1,7 +1,9 @@
-<?php 
+<?php
 $kw = [
-    "INSERT INTO `rezerwacje`(`nr_stolika`, `data_rez`, `liczba_osob`, `telefon`) VALUES ('1','2017-07-04','4','111222333');",//kw1
+    "INSERT INTO `rezerwacje`(`nr_stolika`, `data_rez`, `liczba_osob`, `telefon`) VALUES ('1','2017-07-04','4','111222333');", //kw1
     "SELECT  `nazwa`, `cena` FROM `dania` WHERE `typ` = \'2\';", //kw2
-    "SELECT  `nazwa` FROM `dania` WHERE `typ` = '3' AND `cena` < 15;",//kw3
-    "UPDATE `pracownicy` SET `stanowisko`= '4' WHERE `stanowisko` = '3';"//kw4
+    "SELECT  `nazwa` FROM `dania` WHERE `typ` = '3' AND `cena` < 15;", //kw3
+    "UPDATE `pracownicy` SET `stanowisko`= '4' WHERE `stanowisko` = '3';" //kw4
 ]
+?>
+<!-- kwerendy PHP Array -->
---
+++ PracowniaRepo/exam_ex/p3/rezerwacja.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php $db = mysqli_connect('localhost', 'root', '', 'baza'); ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+
+    <?php
+    if (isset($_POST["data"]) && isset($_POST["person"]) && isset($_POST["number"])) {
+        $data = $_POST["data"];
+        $person = $_POST["person"];
+        $number = $_POST["number"];
+        $qu = mysqli_query($db, "INSERT INTO `rezerwacje`(`nr_stolika`, `data_rez`, `liczba_osob`, `telefon`) VALUES (null,'$data','$person','$number';");
+        if ($qu) {
+            echo "<H1>Dodano rezerwacje do bazy</H1>";
+        }
+        mysqli_close($db);
+    }
+    ?>
+    <script>
+        setTimeout(() => {
+            window.location.replace('http://localhost/Sch_P/PracowniaRepo/exam_ex/p3/restauracja.html')
+        }, 2500)
+    </script>
+</body>
+
+</html>
