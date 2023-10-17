430e605d6f34b5c4701a1c1439ab188a8e908df1
PracowniaRepo <pracownia_repo@outlook.com>
2023-10-17 10:36:06 +0200

    update

--- PracowniaEP/p1_5/script.js
+++ PracowniaEP/p1_5/script.js
@@ -1,11 +1,13 @@
 const obliczanie = () => {
   var x = document.getElementById("num").value;
+  var y = document.getElementById("num_1").value;
+  var z = document.getElementById("num_2").value;
   var pies = document.getElementById("pies").checked;
   var kon = document.getElementById("kon").checked;
   var kot = document.getElementById("kot").checked;
   var cena_1 = pies * 10 * x;
-  var cena_2 = kon * 15 * x;
-  var cena_3 = kot * 5 * x;
+  var cena_2 = kon * 15 * y;
+  var cena_3 = kot * 5 * z;
 
   if (!pies && !kon && !kot) {
     wynik.innerHTML = "wybierz zwierze";
